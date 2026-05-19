70ee976e1d443c0075427809cb8b48628a42f011
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-05-19 09:23:05 +0530

    Time: 23 ms (42.05%) | Memory: 46.8 MB (45.11%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,34 +1,58 @@
+class DisjointSet{
+    public:
+    vector<int> rank,parent;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+      
+        for(int i = 0; i <n;i++){
+            parent[i] = i;
+          
+        }
+    }
+    int findUp(int a){
+        if(parent[a] == a)return a;
+     return parent[a] = findUp(parent[a]);
+    }
+    void UnionByRank(int u, int v){
+        int up_u = findUp(u);
+        int up_v = findUp(v);
+        if(up_u == up_v)return;
+        if(rank[up_u] < rank[up_v]){
+            parent[up_u] = up_v;
+        }
+        else if(rank[up_u] > rank[up_v])
+        parent[up_v] = up_u;
+        else {
+            parent[up_v] = up_u;
+            rank[up_u]++;
+        }
+    }
+
+};
+
+
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size() < n-1)return -1;
-        vector<vector<int>> adj(n);
+        DisjointSet ds(n);
+        int extras = 0;
         for(auto it: connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-        int disconnected = 0;
-        vector<int> vis(n,0);
-        queue<int> q;
-        for(int i = 0;i < n;i++){
-            if(!vis[i]){
-                disconnected++;
-                q.push(i);
-                vis[i] = 1;
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    for(auto it : adj[node]){
-                        if(!vis[it]){
-                            q.push(it);
-                            vis[it] = 1;
-                        }
-                    }
-                }
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUp(u) == ds.findUp(v)){
+                extras++;
+            }
+            else{
+                ds.UnionByRank(u,v);
             }
         }
-
-        return disconnected - 1;
-       
-    }   
+        int components = 0;
+        for(int i =0;i<n;i++){
+            if(ds.parent[i]==i)components++;
+        }
+        int ans = components -1;
+        if(extras >= ans)return ans;
+        return -1;
+    }
 };
