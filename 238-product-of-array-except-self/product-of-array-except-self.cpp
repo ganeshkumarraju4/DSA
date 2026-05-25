3b3a0e8b85ad9f7e6fe58ff72c34dc4da659a90c
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-05-25 09:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (31.33%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> preProductSum(n),sufProductSum(n);
+        int ProductSum = 1;
+        for(int i = 0;i <= n - 1;i++){
+            preProductSum[i] = ProductSum;
+            ProductSum *= nums[i];
+        }
+
+        ProductSum = 1;
+        for(int i = n-1;i>=0;i--){
+            sufProductSum[i] = ProductSum;
+            ProductSum *= nums[i];
+        }
+        vector<int> ans(n);
+        for(int i = 0; i< n;i++){
+            ans[i] = preProductSum[i] * sufProductSum[i];
+        }
+        return ans;
+    }
+};
