11f1098e98e49685112b4f0929bc8159b01befb1
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-07-12 11:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (19.65%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    //memoization
-    int ways(int n,vector<int> &dp){
-        if(n <= 1)return 1;
-        if(dp[n]!= -1)return dp[n];
-        return dp[n] = ways(n-1,dp)+ways(n-2,dp);
-    }
+    //tabulation
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return ways(n,dp);
+        dp[1] = 1;
+        dp[0] = 1;
+
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
