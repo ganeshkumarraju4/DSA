a6742018dea21338d04cf413ff98d7cc66c0f67f
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-07-17 15:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (69.65%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int> &dp,vector<int> &points,int value,int maxValue){
-        if(value > maxValue)return 0;
-
-        if(dp[value]!=-1)return dp[value]; 
-
-        int take = points[value] + solve(dp,points,value+2,maxValue);
-        int not_take = solve(dp,points,value+1,maxValue);
-        return dp[value] = max(take,not_take);
-
-    }
     int deleteAndEarn(vector<int>& nums) {
         int mx = *max_element(nums.begin(),nums.end());
         vector<int> points(mx+1,0);
+        for (int num : nums)
+            points[num] += num;
 
-        for(int i = 0;i<nums.size();i++){
-            points[nums[i]] += nums[i];
+        vector<int> dp(mx + 3, 0);
+
+        for (int value = mx; value >= 1; value--) {
+            int take = points[value] + dp[value + 2];
+            int skip = dp[value + 1];
+            dp[value] = max(take, skip);
         }
-        vector<int> dp(mx+1,-1);
-        return solve(dp,points,0,mx);
-    }
+
+        return dp[1];
+    }   
 };
