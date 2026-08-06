22fd468adb372361e2e63e48e133f17482930e8e
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-08-06 12:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (91.07%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,28 +4,21 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0] = grid[0][0];
+       
 
-        for(int i =0 ;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0)continue;
+        for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        if(i==0 && j==0) continue;
 
-                if(i>0){
-                    if(dp[i][j]){
-                        dp[i][j] = min(dp[i][j],dp[i-1][j]+grid[i][j]);
-                    }
-                    else dp[i][j] = grid[i][j] + dp[i-1][j];
-                }
-                if(j>0){
-                    if(dp[i][j]){
-                        dp[i][j] = min(dp[i][j],dp[i][j-1]+grid[i][j]);
-                    }
-                    else dp[i][j] =grid[i][j] + dp[i][j-1];
-                }
-                cout<<dp[i][j];
-            }
-        }
-        return dp[m-1][n-1];
+        if(i==0)
+            grid[i][j] += grid[i][j-1];
+        else if(j==0)
+            grid[i][j] += grid[i-1][j];
+        else
+            grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+    }
+}
+
+return grid[m-1][n-1];
     }
 };
