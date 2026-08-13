65042553b8c4b25b3ce0d7a1c49a54b187202307
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-08-13 13:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (94.92%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int Solve( vector<vector<int>> &dp,vector<int>& nums,int prevIndex, int i) {
-        if (i >= nums.size()) {
-           
-            return 0;
-        }
-        if(dp[i][prevIndex+1]!=-1)return dp[i][prevIndex+1];
-        int take = 0;
-        
-        if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
-            take = 1 + Solve(dp, nums, i, i + 1);
-        }
-
-        int skip = Solve(dp,nums,prevIndex,i+1);
-       return dp[i][prevIndex+1] = max(take,skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
 
-       
-        return Solve(dp, nums, -1, 0);
+        vector<int> tails;
+
+        for (int x : nums) {
+
+            int pos = lower_bound(tails.begin(), tails.end(), x)
+                      - tails.begin();
+
+            if (pos == tails.size()) {
+                tails.push_back(x);
+            }
+            else {
+                tails[pos] = x;
+            }
+        }
+
+        return tails.size();
     }
 };
