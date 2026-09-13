57b2fa8b38b6f8fc01acaf06033b06a458426ae4
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-09-13 16:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.75%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    bool Check(string &s,vector<vector<int>> &dp,int i,int cnt){
-        if(i==s.size()){
-            if(cnt==0)return 1;
-            return 0;
-        }
-        if(cnt<0)return 0;
-        if(dp[i][cnt]!=-1)return dp[i][cnt];
-        if(s[i]=='(')dp[i][cnt]=Check(s,dp,i+1,cnt+1);
-        if(s[i]==')')dp[i][cnt]=Check(s,dp,i+1,cnt-1);
-
-        if(s[i]=='*'){
-            dp[i][cnt] = Check(s,dp,i+1,cnt+1)||Check(s,dp,i+1,cnt-1)||
-            Check(s,dp,i+1,cnt);
-        }
-        return dp[i][cnt];
-    }
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-
-        return Check(s,dp,0,0);
+        int leftmx = 0, leftmn = 0;
+        for(auto it : s) {
+            if(it == '(') {
+                leftmx++;
+                leftmn++;
+            } else if (it == '*') {
+                leftmx++;
+                leftmn--;
+            } else {
+                leftmx--;
+                leftmn--;
+            }
+            if(leftmx < 0) return false;
+            if(leftmn < 0) leftmn = 0;
+        }
+        return leftmn == 0;
     }
 };
